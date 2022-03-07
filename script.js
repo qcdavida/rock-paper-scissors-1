99444b8a7b7f97d10bbc9ee36c93305ab26abe2a
Ido Rot <idorot9768#gmail.com>
2022-03-07 17:01:51 +0200

    singleRound function

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+
 const wordList= ["Rock","Paper","Scissors"]
 
-function computerPlay(wordList){
+function computerPlay(){
     const computerChoise = wordList[Math.floor(Math.random() *wordList.length)];
     return computerChoise
 }
 
-function userPlay(wordList){
+function userPlay(){
     const userChoise = window.prompt("0 - Rock \n1 - Paper \n2 -Scissors \nPlease pick a number from 0-2:");
     return wordList[userChoise]
 }
 
-console.log(userPlay(wordList))
+function singleRound(){
+
+    const playerSelection = (userPlay(wordList))
+    const computerSelection = (computerPlay(wordList))
+
+    console.log(playerSelection)
+    console.log(computerSelection)
+}
+
